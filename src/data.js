75dadf943b998e6e7b77700e71cd4563a3c7920f
Mylene <pont.mylene@gmail.com>
2021-12-14 17:55:15 +0900

    seeds done

---
+++ src/data.js
@@ -0,0 +1,49 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+    }
+    {
+        title: "Kinosaki-Onsen",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/NpHfabX7Wt4a1VMS7",
+        startDate: "12 Dec, 2019",
+        endDate: "15 Dec, 2019",
+        description: "Quaint resort town in a mountain valley with 7 public hot springs, ryokan inns & Onsenji Temple. The onsen are tatoo-",
+        imageUrl: "https://visitkinosaki.com/vkcore/wp-content/uploads/2021/04/about-top-feat-sp1.jpg"
+    }
+    {
+        title: "Yakushima",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/T68JHkgw8mMTPGXy6",
+        startDate: "05 Oct, 2020",
+        endDate: "08 Oct, 2020",
+        description: "Yakushima is an island in Kagoshima Prefecture, Japan, known for its wildlife and cedar forests. In the northwest is Nagata Inaka-hama, a beach with seasonal loggerhead turtle nesting grounds. The central Mt. Miyanoura is marked by the Arakawa trail and the ancient Jōmon Sugi tree. ",
+        imageUrl: "https://res.cloudinary.com/hnar34gz7/image/upload/w_1280,h_800,c_fill,f_auto,fl_lossy,q_auto/v1/media_ktpo/filer_public/dd/7a/dd7ac046-802e-4d9d-b3a5-e55db5fe1990/07_1_zcw328"
+    }
+    {
+        title: "Ishigaki",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/wabBY2tz1BPswgES9",
+        startDate: "15 Sept, 2020",
+        endDate: "24 Sept, 2020",
+        description: "Ishigaki is a city on Japan’s Ishigaki Island and a jumping-off point for beaches and coral reefs. Near the port, the Misakichō district is home to the covered Euglena Mall. The Yaeyama Museum chronicles the history of Ishigaki and the other Yaeyama Islands.",
+        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+    }
+    {
+        title: "Aomori",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "02 Aug, 2018",
+        endDate: "14 Aug, 2018",
+        description: "Aomori is the capital city of Aomori Prefecture, in the Tōhoku region of Japan. As of 1 April 2020, the city had an estimated population of 278,964 in 136,457 households, and a population density of 340 people per square kilometer spread over the city's total area of 824.61 square kilometers.",
+        imageUrl: "https://www.en-aomori.com/wp-content/uploads/2019/06/c_taishou3.jpg"
+    }
+   
+]
